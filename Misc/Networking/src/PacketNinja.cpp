9df85c8e27b38bde3a1ef80110141257a9aef846
marc-rene <95175463+marc-rene@users.noreply.github.com>
2025-10-14 23:52:43 +0100

    Reshuffling of folder structure

--- Misc/Networking/src/PacketNinja.cpp
+++
@@ -1,8 +0,0 @@
-#include "../include/PacketNinja.hpp"
-
-
-int LINK_TEST_PacketNinja()
-{
-    pn_INFO(SUCCESS_msg);
-    return 43;
-}
--- SegFault Editor/src/Camera.cpp
+++
@@ -1,7 +0,0 @@
-#include "Rendering.hpp"
-#include "Level.hpp"
-
-DirectX::XMMATRIX comp_Camera::GetProjectionMatrix(float p_fov, float p_aspectRatio, float p_nearPlane, float p_farPlane)
-{
-    return DirectX::XMMatrixPerspectiveFovRH(p_fov, p_aspectRatio, this->NearClipPlane, this->FarClipPlane);
-}
